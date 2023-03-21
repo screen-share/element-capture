083d8d45ef3feb1476727abece0733cc59d265f2
Elad Alon <eladalon@google.com>
2023-03-21 13:50:09 +0100

    Add initial version of spec file

---
+++ element-capture.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  specStatus: "unofficial",
+  github: {
+    repoURL: "https://github.com/screen-share/element-capture/",
+    branch: "main",
+  },
+  editors: [
+    {
+      name: "Elad Alon",
+      email: "eladalon@google.com",
+      company: "Google",
+      w3cid: 118124,
+    },
+  ],
+  xref: [
+    "html",
+    "infra",
+    "permissions",
+    "permissions-policy",
+    "dom",
+    "mediacapture-streams",
+    "mediacapture-region",
+    "webidl",
+    "screen-capture",
+  ],
+  subjectPrefix: "[element-capture]",
+};
